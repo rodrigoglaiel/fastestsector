1cb44d53bdfd8edf41fe2ea1d6dcd78f87c06848
Rodrigo Glaiel <109997548+rodrigoglaiel@users.noreply.github.com>
2022-10-02 18:24:56 -0300

    Update TCL GLAIEL.sql

--- TCL GLAIEL.sql
+++ TCL GLAIEL.sql
@@ -11,17 +11,17 @@ set @@autocommit = 1;
 set @@autocommit = 0;
 -- seteo foreign keys en 0 para poder eliminar registros vinculados con otras tablas, luego vuelvo a setear en 1
 set foreign_key_checks = 0;
-select * from universities;
+select * from university;
 
 start transaction;
-delete from universities where id = 1;
-delete from universities where id = 4;
+delete from university where id = 1;
+delete from university where id = 4;
 -- rollback;
 commit;
 
 start transaction;
-insert into universities values (1,'Universidad Nacional de Cordoba','UNC','AR','2017-05-31 14:24:15','2020-03-30 14:49:36');
-insert into universities values (4,'Universidad Catolica de Cordoba','UCC','AR','2017-07-26 16:06:58','2020-03-30 14:49:36');
+insert into university values (1,'Universidad Nacional de Cordoba','UNC',1,'2017-05-31 14:24:15','2020-03-30 14:49:36',null);
+insert into university values (4,'Universidad Catolica de Cordoba','UCC',1,'2017-07-26 16:06:58','2020-03-30 14:49:36',null);
 commit;
 
 set foreign_key_checks = 1;
@@ -30,19 +30,19 @@ set foreign_key_checks = 1;
 Agrega un savepoint a posteriori de la inserción del registro #4 y otro savepoint a posteriori del registro #8
 Agrega en una línea comentada la sentencia de eliminación del savepoint de los primeros 4 registros insertados*/
 
-select * from categories;
+select * from category;
 
 -- vamos a insertar nuevas categorias de documentos
 start transaction;
-insert into categories values (6,'Monografía',now(),now());
-insert into categories values (7,'Ensayo',now(),now());
-insert into categories values (8,'Libro',now(),now());
-insert into categories values (9,'Trabajo científico',now(),now());
+insert into category values (6,'Monografía',now(),now());
+insert into category values (7,'Ensayo',now(),now());
+insert into category values (8,'Libro',now(),now());
+insert into category values (9,'Trabajo científico',now(),now());
 savepoint primer_tanda;
-insert into categories values (10,'Tesis',now(),now());
-insert into categories values (11,'Tesina',now(),now());
-insert into categories values (12,'Ejercicios prácticos',now(),now());
-insert into categories values (13,'Paper',now(),now());
+insert into category values (10,'Tesis',now(),now());
+insert into category values (11,'Tesina',now(),now());
+insert into category values (12,'Ejercicios prácticos',now(),now());
+insert into category values (13,'Paper',now(),now());
 savepoint segunda_tanda;
 -- release savepoint primer_tanda;
 commit;
